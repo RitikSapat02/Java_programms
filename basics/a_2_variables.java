7af4f7573f2d366322bfcd84d69676bf744d1ff9
Ritik Sapat <118529770+RitikSapat02@users.noreply.github.com>
2022-11-26 18:21:19 +0530

    Add files via upload

---
+++ basics/a_2_variables.java
@@ -0,0 +1,21 @@
+public class a_2_variables {
+    public static void main(String[] args) {
+        int age = 37;
+        String name = "sandeep";
+        float weight = 70.5f;
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(weight);
+
+        // constant
+        final int MAX_AGE = 200;
+        final int cost = 200;
+        System.out.print(cost);
+        // MAX_AGE = 4;
+        System.out.println(MAX_AGE);
+
+        // declarig many variables
+        int a = 1, b = 3, c = 6;
+        System.out.println(a + b + c);
+    }
+}
