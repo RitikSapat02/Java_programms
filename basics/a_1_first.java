71570ce95aac32cc1977a574a01b846b96e1b4be
Ritik Sapat <118529770+RitikSapat02@users.noreply.github.com>
2022-11-26 18:19:25 +0530

    Add files via upload

---
+++ basics/a_1_first.java
@@ -0,0 +1,5 @@
+public class a_1_first {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ basics/a_3_primitive.java
@@ -0,0 +1,27 @@
+public class a_3_primitive {
+    public static void main(String[] args) {
+        byte a = 90;
+        short b = 2;
+        int c = 500;
+        long myn = 249239422323L;
+        float f = 6f;
+        boolean isvalid = true;
+        char g = 'M';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(myn);
+        System.out.println(f);
+        System.out.println(isvalid);
+        System.out.println(g);
+
+        float flo = 35e3f;
+        System.out.println(flo);
+        double gg = 34e2d;
+        System.out.println(gg);
+
+        char r = 65;
+        System.out.println(r);
+
+    }
+}
